198f1e9e22c6ae8fcc17d6e8cb916b64c1335185
Jeremy Bozarth <jbozarth@polarisio.com>
2026-04-28 11:40:19 -0500

    Made db wrapper more type safe

--- db.py
+++ db.py
@@ -1,15 +1,21 @@
 from report import *
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Engine
+from sqlalchemy.engine import Connection
+from typing import Optional
 
-_engine = ()
-_conn = ()
+_engine: Optional[Engine] = None
+_conn: Optional[Connection] = None
 
-def get_engine():
+def get_engine() -> Engine:
+    if _engine is None:
+        raise RuntimeError("Database engine not initialized")
 
     return _engine
 
 
-def get_conn():
+def get_conn() -> Connection:
+    if _conn is None:
+        raise RuntimeError("Database connection not initialized")
 
     return _conn
 
@@ -24,4 +30,9 @@ def connect_to_db(conn_url):
 
 
 def close_db_connection():
-    _conn.close()
+
+    global _conn
+
+    if _conn is not None:
+        _conn.close()
+        _conn = None
